cc1c8d864cd6f4ae4b95dfee3f153cad701c9522
Paul Sladek <paul.sladek@warnerbros.com>
2020-06-08 17:52:39 -0700

    mutli changes

--- multi-account.sh
+++ multi-account.sh
@@ -5,14 +5,33 @@
 cmd=$1
 region=${2:-us-west-2}
 
-[ "$#" -eq 1 ] || exit "1 argument required, $# provided"
+if [ $# -le 1 ]; then
+    echo "Usage: ./multi-account.sh \"aws s3 ls\""
+    exit 1
+fi
 
-IFS=$'\n'
-for aws_profile in $(awsp)
-do
-    export AWS_PROFILE="$aws_profile"
-    export AWS_REGION="$region"
-    echo "Using $aws_profile aws profile"
-    echo "Running: $cmd"
-    eval "$cmd"
-done
+if [[ "$region" == "all" ]]; then
+  for region in $(aws ec2 describe-regions --query 'Regions[].RegionName' --output text)
+  do
+    IFS=$'\n'
+    for aws_profile in $(awsp)
+    do
+      export AWS_PROFILE="$aws_profile"
+      export AWS_REGION="$region"
+      echo "Using $aws_profile aws profile"
+      echo "Running: $cmd"
+      echo "Running: $region"
+      eval "$cmd"
+    done
+  done
+else
+  IFS=$'\n'
+  for aws_profile in $(awsp)
+  do
+      export AWS_PROFILE="$aws_profile"
+      export AWS_REGION="$region"
+      echo "Using $aws_profile aws profile"
+      echo "Running: $cmd"
+      eval "$cmd"
+  done
+fi
